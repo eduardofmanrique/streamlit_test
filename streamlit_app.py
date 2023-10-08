6fb2def2239c92b620e31519c1369fe7e62615ed
eduardofmanrique <71049903+eduardofmanrique@users.noreply.github.com>
2023-10-08 19:03:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,30 +14,30 @@ data_sources = [
     # Add more data sources as needed
 ]
 
-def select_data_sources():
-    st.title("Select Data Sources")
-    selected_sources = st.multiselect(
-        "Select Data Sources",
-        options=[source.csv_name for source in data_sources]
-    )
-    
-    if selected_sources:
-        st.write("Selected Data Sources:")
-        for source in data_sources:
-            if source.csv_name in selected_sources:
-                st.write(source.csv_name)
-    
-    # Add a button to load the selected data sources
-    if st.button("Load Selected Data"):
-        selected_data = [source.df() for source in data_sources if source.csv_name in selected_sources]
-        
-        # You can now work with the selected data as needed
-        # For example, you can concatenate the selected DataFrames:
-        if selected_data:
-            combined_data = pd.concat(selected_data)
-            st.write("Combined Data:")
-            st.write(combined_data)    st.write("Selected Data Sources:")
+
+st.title("Select Data Sources")
+selected_sources = st.multiselect(
+    "Select Data Sources",
+    options=[source.csv_name for source in data_sources]
+)
+
+if selected_sources:
+    st.write("Selected Data Sources:")
     for source in data_sources:
         if source.csv_name in selected_sources:
-            st.write(source.df())
+            st.write(source.csv_name)
+
+# Add a button to load the selected data sources
+if st.button("Load Selected Data"):
+    selected_data = [source.df() for source in data_sources if source.csv_name in selected_sources]
+    
+    # You can now work with the selected data as needed
+    # For example, you can concatenate the selected DataFrames:
+    if selected_data:
+        combined_data = pd.concat(selected_data)
+        st.write("Combined Data:")
+        st.write(combined_data)    st.write("Selected Data Sources:")
+for source in data_sources:
+    if source.csv_name in selected_sources:
+        st.write(source.df())
 
