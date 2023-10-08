fa39055254d83a549eefec1aab22a54014f58c28
eduardofmanrique <71049903+eduardofmanrique@users.noreply.github.com>
2023-10-08 19:03:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import streamlit as st
-
-# Define your Streamlit app
 import streamlit as st
 import pandas as pd
 
@@ -13,20 +9,35 @@ class ReadCSV():
         return pd.read_csv(self.csv_name)
 
 data_sources = [
-    ReadCSV("data_1.csv"),
-    ReadCSV("data_2.csv"),
+    ReadCSV("data_source_1.csv"),
+    ReadCSV("data_source_2.csv"),
     # Add more data sources as needed
 ]
 
-
-st.title("Select Data Sources")
-selected_sources = st.multiselect(
-    "Select Data Sources",
-    options=[source.csv_name for source in data_sources]
-)
-if selected_sources:
-    st.write("Selected Data Sources:")
+def select_data_sources():
+    st.title("Select Data Sources")
+    selected_sources = st.multiselect(
+        "Select Data Sources",
+        options=[source.csv_name for source in data_sources]
+    )
+    
+    if selected_sources:
+        st.write("Selected Data Sources:")
+        for source in data_sources:
+            if source.csv_name in selected_sources:
+                st.write(source.csv_name)
+    
+    # Add a button to load the selected data sources
+    if st.button("Load Selected Data"):
+        selected_data = [source.df() for source in data_sources if source.csv_name in selected_sources]
+        
+        # You can now work with the selected data as needed
+        # For example, you can concatenate the selected DataFrames:
+        if selected_data:
+            combined_data = pd.concat(selected_data)
+            st.write("Combined Data:")
+            st.write(combined_data)    st.write("Selected Data Sources:")
     for source in data_sources:
         if source.csv_name in selected_sources:
-            st.write(source.csv_name)
+            st.write(source.df())
 
